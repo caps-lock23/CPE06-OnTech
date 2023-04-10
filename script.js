750fd0c6dad344e07cc1fb9f0cc9d13e981c331c
caps-lock23 <93170439+caps-lock23@users.noreply.github.com>
2023-04-10 12:58:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function filterProducts() {
     //for each product of products query title, price, & description
     products.forEach((product) => {
         //store title, price, description
-        const title = product.querySelector('figcaption h4').textContent.toLowerCase();
-        const price = product.querySelector('figcaption p.priceTag').textContent.toLowerCase();
-        const description = product.querySelector('figcaption p').textContent.toLowerCase();
+        const titleElement = product.querySelector('figcaption h4');
+        const priceElement = product.querySelector('figcaption p.priceTag');
+        const descriptionElement = product.querySelector('figcaption p');
+        
+        // check if title, price, and description elements exist before accessing their textContent property
+        const title = titleElement ? titleElement.textContent.toLowerCase() : '';
+        const price = priceElement ? priceElement.textContent.toLowerCase() : '';
+        const description = descriptionElement ? descriptionElement.textContent.toLowerCase() : '';
 
         //diplay block if search input matches
         if (title.includes(searchBox) || price.includes(searchBox) || description.includes(searchBox)) {
