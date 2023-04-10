94d9d87b3a77e81d5208d03bcb5af218f337546b
caps-lock23 <93170439+caps-lock23@users.noreply.github.com>
2023-04-10 12:39:38 +0800

    added search by description and price

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function filterProducts() {
+    //store search input and products
     const searchBox = document.querySelector('#search-box').value.toLowerCase();
     const products = document.querySelectorAll('#product-list li');
 
+    //for each product of products query title, price, & description
     products.forEach((product) => {
+        //store title, price, description
         const title = product.querySelector('figcaption h4').textContent.toLowerCase();
+        const price = product.querySelector('figcaption p.priceTag').textContent.toLowerCase();
+        const description = product.querySelector('figcaption p').textContent.toLowerCase();
 
-        if (title.includes(searchBox)) {
+        //diplay block if search input matches
+        if (title.includes(searchBox) || price.includes(searchBox) || description.includes(searchBox)) {
             product.style.display = 'block';
         } else {
             product.style.display = 'none';
