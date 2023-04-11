bf66be50550681cc1cf105afdefca2b2442f5698
caps-lock23 <93170439+caps-lock23@users.noreply.github.com>
2023-04-11 20:04:22 +0800

    removed ternary check, updated the code

--- script.js
+++ script.js
@@ -3,19 +3,12 @@ function filterProducts() {
     const searchBox = document.querySelector('#search-box').value.toLowerCase();
     const products = document.querySelectorAll('#product-list li');
 
-    //for each product of products query title, price, & description
     products.forEach((product) => {
-        //store title, price, description
-        const titleElement = product.querySelector('figcaption h4');
-        const priceElement = product.querySelector('figcaption p.priceTag');
-        const descriptionElement = product.querySelector('figcaption p');
+        //query title, price, & description for each product
+        const title = product.querySelector('h4').textContent.toLowerCase();
+        const price = product.querySelector('p.priceTag').textContent.toLowerCase();
+        const description = product.querySelector('p').textContent.toLowerCase();
 
-        // check if title, price, and description elements exist before accessing their textContent property
-        const title = titleElement ? titleElement.textContent.toLowerCase() : '';
-        const price = priceElement ? priceElement.textContent.toLowerCase() : '';
-        const description = descriptionElement ? descriptionElement.textContent.toLowerCase() : '';
-
-        //diplay block if search input matches
         if (title.includes(searchBox) || price.includes(searchBox) || description.includes(searchBox)) {
             product.style.display = 'block';
         } else {
